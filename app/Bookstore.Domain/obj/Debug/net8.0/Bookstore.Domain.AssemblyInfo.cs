a4c4b4137fa272b52df89f7db86474244a84442e
NoReply Amazon <no-reply@amazon.com>
2024-12-17 17:23:34 +0000

    [Commit] Iteration 5 #errors = 72 <= 73.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Helpers/IOwinRequestExtensions.cs][CS0246][8,48] The type or namespace name 'IOwinRequest' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Domain/obj/Debug/net8.0/Bookstore.Domain.AssemblyInfo.cs
+++ app/Bookstore.Domain/obj/Debug/net8.0/Bookstore.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e28521d1930023db9d2ed0886a86461c3df07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3166185e8232cd47ffae73a8b71a57cb53ccd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
