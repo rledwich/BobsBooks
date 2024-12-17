11642476f207fad45fa0226de7d15a4c3f817b90
NoReply Amazon <no-reply@amazon.com>
2024-12-17 17:29:03 +0000

    [Commit] Iteration 15 #errors = 18 <= 30.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryIndexViewModel.cs][CS0246][19,28] The type or namespace name 'SelectListItem' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Cdk/obj/Debug/net8.0/Bookstore.Cdk.AssemblyInfo.cs
+++ app/Bookstore.Cdk/obj/Debug/net8.0/Bookstore.Cdk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Cdk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee6ca6a36cda3e6023c3099e87f4b3bce880dc31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415fc85c08e6cbd36c853efe2d4611ecc6522328")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Cdk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Cdk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
