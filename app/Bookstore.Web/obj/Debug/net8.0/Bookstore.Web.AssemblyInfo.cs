6bcf8a6e3b2dc22023337c1cb08b05d749f9b1dd
NoReply Amazon <no-reply@amazon.com>
2024-12-17 17:33:21 +0000

    [Commit] Iteration 21 #errors = 2 <= 5.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Helpers/HttpContextExtensions.cs][CS0246][23,13] The type or namespace name 'HttpCookie' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/obj/Debug/net8.0/Bookstore.Web.AssemblyInfo.cs
+++ app/Bookstore.Web/obj/Debug/net8.0/Bookstore.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+514128e56ee13aa9e87cab517c7a3f65467dcf31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ab9c91faf66e80940b563230ff9e201f11ee25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
