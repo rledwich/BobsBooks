12ee06bb23cb7ff2d87414f0fbbf3592362710b6
NoReply Amazon <no-reply@amazon.com>
2024-12-17 17:28:31 +0000

    [Commit] Iteration 14 #errors = 30 <= 34.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Helpers/MvcHelpers.cs][CS0246][13,80] The type or namespace name 'HtmlHelper' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/obj/Debug/net8.0/Bookstore.Web.AssemblyInfo.cs
+++ app/Bookstore.Web/obj/Debug/net8.0/Bookstore.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78f0def49c8ef47cff88dc18bf3aae21ffa3d7a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee6ca6a36cda3e6023c3099e87f4b3bce880dc31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
